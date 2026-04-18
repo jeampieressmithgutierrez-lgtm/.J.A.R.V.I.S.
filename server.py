e5ebe7063531fb756003fb3ce1a19eff281d8fe3
jeampieressmithgutierrez-lgtm <jeampieressmithgutierrez@gmail.com>
2026-04-18 18:23:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
-from flask import Flask, send_file, request, jsonify
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return send_file("index.html")
-
-@app.route("/chat", methods=["POST"])
-def chat():
-    data = request.json
-    mensaje = data.get("mensaje", "")
-    return jsonify({"respuesta": f"Señor, usted dijo: {mensaje}"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
